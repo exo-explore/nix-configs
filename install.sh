c5b320148a7f796d5752694b629555ce2bad0bf6
Evan <evanev7@gmail.com>
2025-10-03 12:48:03 +0100

    install is conditioned on existing nix-configs directory

--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ else
         bash <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
 fi
 cd ~ || exit 1
-git clone https://github.com/exo-explore/nix-configs
-cd nix-configs || exit 1
+if [ -d ~/nix-configs/.git ]; then
+        cd nix-configs || exit 1
+        git pull
+else
+        git clone https://github.com/exo-explore/nix-configs
+        cd nix-configs || exit 1
+fi
 sudo nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch --flake .#"$(whoami)"
 gh auth login
