4ecd7697c314e630ba2ba54976d9329621ee97af
Evan <evanev7@gmail.com>
2025-10-03 13:04:08 +0100

    no need to get nix from environment

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-if [ -d ~/nix-configs/.git ]; then
+set -euo pipefail
+if [ -f ~/nix-configs/flake.lock ]; then
         cd ~/nix-configs || exit 1
-        git pull
 else
         git clone https://github.com/exo-explore/nix-configs ~/nix-configs
         cd ~/nix-configs || exit 1
 fi
-if nix --version >/dev/null 2>&1; then
+NIX=/nix/var/nix/profiles/default/bin/nix
+if $NIX --version >/dev/null 2>&1; then
         echo "Nix already installed"
 else
         echo "Installing nix"
         bash <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
 fi
-sudo nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch --flake .#"$(whoami)"
+sudo $NIX --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch --flake .#"$(whoami)"
 gh auth login
